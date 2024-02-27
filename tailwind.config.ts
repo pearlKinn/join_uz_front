79851accebf0aaca2521744c74abb9ae4fb458da
김진주 <83470333+pearlKinn@users.noreply.github.com>
2024-02-27 14:51:55 +0900

    :art: [Style] tailwind 컬러칩 세팅 (#8)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,60 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {},
+      colors: {
+        /* Colors */
+        /* cyan */
+        cyan50: "#ECFEFF",
+        cyan100: "#D5FCFF",
+        cyan200: "#B1F9FF",
+        cyan300: "#8BECF2",
+        cyan400: "#53DBEC",
+        cyan500: "#3BC9DB",
+        cyan600: "#1DADBE",
+        cyan700: "#1898A7",
+        cyan800: "#13808D",
+        cyan900: "#0F6772",
+        /* orange */
+        orange50: "#FFF7ED",
+        orange100: "#FFEDD5",
+        orange200: "#FED7AA",
+        orange300: "#FDBA74",
+        orange400: "#FB923C",
+        orange500: "#F97316",
+        orange600: "#EA580C",
+        orange700: "#EA580C",
+        orange800: "#9A3412",
+        orange900: "#7C2D12",
+        /* gray */
+        gray50: "##F9FAFB",
+        gray100: "#F3F4F6",
+        gray200: "#E5E7EB",
+        gray300: "#D2D5DA",
+        gray400: "#9CA3AF",
+        gray500: "#6D7280",
+        gray600: "#4B5563",
+        gray700: "#374151",
+        gray800: "#1F2937",
+        gray900: "#111827",
+        /** error */
+        error: "#E14646",
+        errorText: "#C20C0C",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
