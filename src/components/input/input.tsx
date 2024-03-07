aabeafcc5f91e7eb6f412118030a5a362ad3c151
Pearl Kim <jinju000202@gmail.com>
2024-03-07 14:10:57 +0900

    :sparkles: [Feature] input 컴포넌트 생성

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -13,22 +13,33 @@ const Input: React.FC<InputProps> = ({ id, placeholder, type }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
-  const input = isVisible ? "password" : "text";
   const visibility = isVisible ? (
-    <Image
-      src="/Icon/Eye/OffIcon.svg"
-      width={24}
-      height={24}
-      alt="비밀번호 숨기기"
-    />
-  ) : (
     <Image
       src="/Icon/Eye/OnIcon.svg"
       width={24}
       height={24}
       alt="비밀번호 보기"
     />
+  ) : (
+    <Image
+      src="/Icon/Eye/OffIcon.svg"
+      width={24}
+      height={24}
+      alt="비밀번호 숨기기"
+    />
   );
+
+  const handleTypeChange = () => {
+    if (id === "ID") {
+      // ID일 경우에는 항상 text로 고정되며, maxLength는 12입니다.
+      return "text";
+    } else if (id === "password") {
+      // password일 경우에는 버튼을 클릭할 때마다 text와 password를 왔다갔다 합니다.
+      return isVisible ? "text" : "password";
+    } else {
+      return type;
+    }
+  };
 
   return (
     <div className="w-[329px] h-[74px] relative">
@@ -37,7 +48,7 @@ const Input: React.FC<InputProps> = ({ id, placeholder, type }) => {
       </label>
       <input
         className="w-full h-[56px] pt-[10px] pb-[14px] pr-10 focus:outline-none border-b border-b-gray300 focus:border-b-primary600"
-        type={input}
+        type={handleTypeChange()}
         name={id}
         id={id}
         placeholder={placeholder}
